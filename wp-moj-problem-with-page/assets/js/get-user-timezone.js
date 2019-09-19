86dc69a9954a27268f3c8b3ca7abc9e21724f7cc
Stephanie David <stephanie.david@digital.justice.gov.uk>
2019-09-19 12:06:35 +0100

    Add files via upload

---
+++ wp-moj-problem-with-page/assets/js/get-user-timezone.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var d = new Date();
+    var n = d.getTimezoneOffset();
+    document.getElementById("timezone").innerHTML = n;
+}
+// n.b. the returned value is not a constant, because of the practice of using Daylight Saving Time.
+// From the end of March to the end of October the offset for the UK(GMT) will display as -60
