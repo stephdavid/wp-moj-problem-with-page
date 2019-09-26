fb9aeee5f89bdadca204018a866665fb29fc0d36
Stephanie David <stephanie.david@digital.justice.gov.uk>
2019-09-26 09:52:29 +0100

    Add files via upload

--- pwtp-form.php
+++ pwtp-form.php
@@ -1,5 +1,4 @@
 <?php
-
 // disable direct access
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -14,16 +13,21 @@ function pwtp_scripts() {
 add_action( 'wp_enqueue_scripts', ' add_pwtp_scripts' );
 
 // contact form
-$email_form = '<form getinfo() id="pwtp" class="'.$pwtp_atts['class'].'" method="post">
+	$email_form = '<form id="pwtp" class="'.$pwtp_atts['class'].'" method="post">
 	<div class="form-group pwtp-problem-group">
-		<label for="pwtp_problem">'.esc_attr($problem_label).' <span class="'.(isset($error_class['form_problem']) ? "pwtp-error" : "pwtp-hide").'" >'.esc_attr($error_problem_label).'</span></label>
-		<textarea name="pwtp_problem" id="pwtp_problem" rows="20" '.(isset($error_class['form_message']) ? ' class="form-control pwtp-error"' : ' class="form-control"').'>'.esc_textarea($form_data['form_message']).'</textarea>
+		<label for="pwtp_problem">'.esc_attr($problem_label).': <span class="'.(isset($error_class['form_problem']) ? "pwtp-error" : "pwtp-hide").'" >'.esc_attr($error_problem_label).'</span></label>
+		<input type="text" name="pwtp_problem" id="pwtp_name" '.(isset($error_class['form_name']) ? ' class="form-control pwtp-error"' : ' class="form-control"').' value="'.esc_attr($form_data['form_name']).'" />
 	</div>
+
 	<div class="form-group pwtp-improvement-group">
 		<label for="pwtp_improvement">'.esc_attr($improvement_label).' <span class="'.(isset($error_class['form_message']) ? "pwtp-error" : "pwtp-hide").'" >'.esc_attr($error_message_label).'</span></label>
 		<textarea name="pwtp_improvement" id="pwtp_improvement" rows="20" '.(isset($error_class['form_message']) ? ' class="form-control pwtp-error"' : ' class="form-control"').'>'.esc_textarea($form_data['form_message']).'</textarea>
 	</div>
 
+	<div class="form-group pwtp-hide">
+		'.$pwtp_nonce_field.'
+	</div>
+	
 	<div class="form-group pwtp-submit-group">
 		<button type="submit" name="'.$submit_name_id.'" id="'.$submit_name_id.'" class="btn btn-primary">'.esc_attr($submit_label).'</button>
 	</div>
@@ -37,4 +41,3 @@ $email_form = '<form getinfo() id="pwtp" class="'.$pwtp_atts['class'].'" method=
 	<input type="hidden" id="language" name="language" value="">
 	<input type="hidden" id="timezone" name="timezone" value="">
 </form>';
-
