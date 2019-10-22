407393d069f75caec8121517120bd831b3109e6f
Stephanie David <stephanie.david@digital.justice.gov.uk>
2019-10-22 19:40:18 +0100

    Add files via upload

--- pwtp-form.php
+++ pwtp-form.php
@@ -15,8 +15,6 @@ add_action( 'wp_enqueue_scripts', ' add_pwtp_scripts' );
 // contact form
 	$email_form = '<form id="pwtp" class="'.$pwtp_atts['class'].'" method="post">
 
-
-
 	<div class="form-group pwtp-problem-group">
 		<label for="pwtp_problem">'.esc_attr($problem_label).': <span class="'.(isset($error_class['form_problem']) ? "pwtp-error" : "pwtp-hide").'" >'.esc_attr($error_problem_label).'</span></label>
 		<textarea name="pwtp_problem" id="pwtp_problem" rows="20" '.(isset($error_class['form_problem']) ? ' class="form-control pwtp-error"' : ' class="form-control"').'>'.esc_textarea($form_data['form_problem']).'</textarea>
