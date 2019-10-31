88c551621892695c041b03eb7c237edec47696f1
Stephanie David <stephanie.david@digital.justice.gov.uk>
2019-10-31 12:37:19 +0000

    Add files via upload

--- pwtp-form.php
+++ pwtp-form.php
@@ -10,7 +10,6 @@ function pwtp_scripts() {
     wp_enqueue_script('wp-moj-problem-with-this-page-scripts');
 }
 add_action( 'wp_enqueue_scripts', ' add_pwtp_scripts' );
-
 ?>'<p><em>Help us improve the experience for others. Please don’t include any personal information.</em></p>
 <?php
 // contact form
@@ -35,7 +34,20 @@ $email_form = '<form id="pwtp" class="'.$pwtp_atts['class'].'" method="post">
 	<button type="submit" name="'.$submit_name_id.'" id="'.$submit_name_id.'" class="btn btn-primary">'.$submit_label.'</button>
 </div>
 
-	<input type="hidden" id="prev_url" name="prev_url" value="'.$_SERVER['HTTP_REFERER'];'">
-	<input type="hidden" id="browser" name="browser" value="'.$_SERVER['HTTP_USER_AGENT'];'">
-	<input type="hidden" id="time" name="time" value="'.$_SERVER['REQUEST_TIME'];'">
+	<DIV>
+
+	<input type="hidden" id="prev_url" name="prev_url" value="'.$_SERVER['HTTP_REFERER'].'">
+	<input type="hidden" id="url" name="url" value="">
+	<input type="hidden" id="browser" name="browser" value="">
+	<input type="hidden" name="version" id="version" value="">
+	<input type="hidden" id="res" name="res" value="">
+	<input type="hidden" id="os" name="os" value="">
+	<input type="hidden" id="useragent" name="useragent" value="">
+	<input type="hidden" id="language" name="language" value="">
+	<input type="hidden" id="timezone" name="timezone" value="">
+
+</DIV>
+
 </form>';
+
+// $value = $_POST['value'] ?? '';
--- pwtp-submission.php
+++ pwtp-submission.php
@@ -3,18 +3,33 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
 if (isset($_POST['prev_url'])) {
 	$prev_url = $_POST['prev_url'];
 }
 
-//if (isset($_POST['browser'])) {
-//	$browser = $_POST['browser'];
-//}
+if (isset($_POST['browser'])) {
+	$browser = $_POST['browser'];
+}
 
-//if (isset($_POST['time'])) {
-//	$time = $_POST['time'];
-//}
+if (isset($_POST['os'])) {
+	$os = $_POST['os'];
+}
+
+if (isset($_POST['version'])) {
+	$version = $_POST['version'];
+}
+
+if (isset($_POST['language'])) {
+	$language = $_POST['language'];
+}
+
+if (isset($_POST['res'])) {
+	$res = $_POST['res'];
+}
+
+if (isset($_POST['timezone'])) {
+	$timezone = $_POST['timezone'];
+}
 
 // sending and saving form submission
 if ($error == false) {
@@ -48,22 +63,26 @@ if ($error == false) {
 	} else {
 		$subject = $prefix;
 	}
-
-
 	// mail
 	$the_problem = $form_data['form_problem'];
 	$the_improvement = $form_data['form_improvement'];
-
 	//$to = $email_admin;
 	$subject = "PWTP message from the $blog_name website" ;
 	$content = "<h1>Problem with this page</h1><p>A problem has been reported by a visitor to the <strong>$blog_name</strong> website on this page: <strong>$prev_url</strong></p>" . 
+	
 	sprintf( "<h2>What were you doing and what went wrong?</h2> %s ", $the_problem )  .
-	sprintf( "<h2>What could we improve?</h2> %s ", $the_improvement ) . "<h2>Browser Data:</h2>"// . 
-	//sprintf( "<ol><li> %s ", $browser ) . "</li>" .
-	//sprintf( "<li> %s ", $timezone_offset ) . "</li></ol>"
+	sprintf( "<h2>What could we improve?</h2> %s ", $the_improvement ) . 
+	"<h2>Browser Data:</h2>
+	<ol>
+	<li>Browser: " . $browser . "</li>
+	<li>Operating System: " . $os . "</li>
+	<li>Version: " . $version . "</li> 
+	<li>Language: " . $language . "</li> 
+	<li>Resolution: " . $res . "</li> 
+	<li>Timezone: " . $timezone . "</li>
+	<li>User Agent: " . $_SERVER['HTTP_USER_AGENT'] . "</li></ol>"
 	;
 	$headers = "Content-Type: text/html; charset=UTF-8";
-
 	if( wp_mail($to, wp_strip_all_tags($subject), $content, $headers) ) {
 		$sent = true;
 	} else {
